0bdbe0f5f70e6174c20fc4583adaa584cc74415b
Brenton Forwood <forwoodb77@gmail.com>
2026-05-03 04:35:03 -0400

    Connected to fastapi

--- app/api/optimize/route.ts
+++ app/api/optimize/route.ts
@@ -1,8 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
-  console.log(body);
 
-  return NextResponse.json(body);
+  const res = await fetch("http://127.0.0.1:8000/optimize", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+  // console.log(data);
+
+  return NextResponse.json(data);
 }
