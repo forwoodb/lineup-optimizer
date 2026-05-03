802968fda923fd9f00c5671f94b14f78faa0195a
Brenton Forwood <forwoodb77@gmail.com>
2026-05-03 03:47:54 -0400

    Created button to send player data to api, created api route 'optimize'

--- app/components/PlayersTable.tsx
+++ app/components/PlayersTable.tsx
@@ -7,36 +7,48 @@ interface PlayersTableProps {
 }
 
 const PlayersTable = ({ csvData }: PlayersTableProps) => {
+  const generateLineup = async () => {
+    await fetch("/api/optimize", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(csvData),
+    });
+    console.log("click");
+  };
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Position</th>
-          <th>Name</th>
-          <th>Salary</th>
-          <th>Game Info</th>
-          <th>Team</th>
-          <th>APPG</th>
-        </tr>
-      </thead>
-      <tbody>
-        {csvData.map((player) => {
-          return (
-            <tr key={player.ID}>
-              <td>
-                <input type="checkbox" name="lock" id="lock" />
-              </td>
-              <td>{player.Position}</td>
-              <td>{player.Name}</td>
-              <td>{player.Salary}</td>
-              <td>{player["Game Info"]}</td>
-              <td>{player.TeamAbbrev}</td>
-              <td>{player.AvgPointsPerGame}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <button onClick={generateLineup}>Generate Lineup</button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Salary</th>
+            <th>Game Info</th>
+            <th>Team</th>
+            <th>APPG</th>
+          </tr>
+        </thead>
+        <tbody>
+          {csvData.map((player) => {
+            return (
+              <tr key={player.ID}>
+                <td>
+                  <input type="checkbox" name="lock" id="lock" />
+                </td>
+                <td>{player.Position}</td>
+                <td>{player.Name}</td>
+                <td>{player.Salary}</td>
+                <td>{player["Game Info"]}</td>
+                <td>{player.TeamAbbrev}</td>
+                <td>{player.AvgPointsPerGame}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
