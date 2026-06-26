91207a38cfe723e95aeb4744e490e1877b186ca8
Brenton Forwood <forwoodb77@gmail.com>
2026-06-26 04:28:39 -0400

    Created LpProblem variable

--- app/python/optimizer.py
+++ app/python/optimizer.py
@@ -8,5 +8,7 @@ csv_dk = pd.read_csv('./csv-data/DKSalaries.csv')
 # convert csv data to dataframe
 df_dk = pd.DataFrame(csv_dk)
 
+prob = LpProblem('MLB', LpMaximize)
+
 pd.set_option('display.max_columns', None)
 print(df_dk)
